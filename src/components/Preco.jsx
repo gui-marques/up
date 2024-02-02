4bf2d1f2517a5c6edac282d8572bce9726b4261f
ATIBrasil <gui.slayon@live.com>
2024-02-02 16:02:02 -0300

    02-02-2024 16h

--- src/components/Preco.jsx
+++ src/components/Preco.jsx
@@ -30,30 +30,30 @@ const Preco = (props) => {
                 tracking-wide text-white shadow-md">{plan.mostPopular}</p>
                 )}
 
-                <p className="mt-2 text-sm leading-6 text-slate-300">
+                <p className="mt-2 text-sm text-[#907c3a]  leading-6 text-slate-300">
                   {plan.description}
                 </p>{/* Aqui fica os preços */}
                 <div className="-mx-8 mt-0 rounded-lg p-0">
                   
-                  <span className="text-sm font-semibold ">{plan.cifra}</span>
+                  <span className="text-sm font-semibold">{plan.cifra}</span>
                   <span className="ml-1 text-4xl">{plan.preco}</span>
-                  <span className="ml-1.5">{plan.frequency}</span>
+                  <span className="ml-1.5 ">{plan.frequency}</span>
                 </div>
                     {/* Aqui acaba os preços */}
                 <ul className="mt-4  space-y-1">
                   {plan.features.map((feature) => (
                     <li
-                      key={feature} className="text-base leading-6"
+                      key={feature} className="text-base   leading-6"
                     >
 
                       <svg
-                      className="float-left w-5 h-5 text-accent " 
+                      className="float-left w-5 h-5  text-accent " 
                       version="1.1" id="Layer_1" 
                       xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 122.88 109.76" >
 
                       <path class="st0" d="M0,52.88l22.68-0.3c8.76,5.05,16.6,11.59,23.35,19.86C63.49,
                       43.49,83.55,19.77,105.6,0h17.28 C92.05,34.25,66.89,70.92,46.77,109.76C36.01,86.69,20.96,
-                      67.27,0,52.88L0,52.88L0,52.88z" fill="currentColor" />
+                      67.27,0,52.88L0,52.88L0,52.88z" fill="#907c3a" />
                       </svg>
 
                       <span className="ml-0">{feature}</span>
